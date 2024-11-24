b3d1d7745030f022d49ae6229d2ea465610eb213
rodes nzie <rodesnzie@gmail.com>
2024-11-24 22:35:36 +0100

    rename companies and categories Model files

---
+++ app/models/companies.ts
@@ -0,0 +1,33 @@
+import { DateTime } from 'luxon'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import { BaseModel, belongsTo, column } from '@adonisjs/lucid/orm'
+import User from './user.js'
+
+export default class Company extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare slug: string
+
+  @column()
+  declare name: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare cover: string
+
+  @column()
+  declare userId: BelongsTo<typeof User>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+}
