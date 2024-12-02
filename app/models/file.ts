a5141f1480c2a20b14e8a4817298f3fb03aad17a
rodes nzie <rodesnzie@gmail.com>
2024-12-02 11:01:11 +0100

    create files Model

---
+++ app/models/file.ts
@@ -0,0 +1,25 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class File extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare slug: string
+  
+  @column()
+  declare url: string
+  
+  @column()
+  declare title: string
+
+  @column()
+  declare type: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
