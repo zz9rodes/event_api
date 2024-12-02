41aafa65e553b2016aa1fe63b4e61de8e06f574f
rodes nzie <rodesnzie@gmail.com>
2024-12-02 23:52:07 +0100

    posts migration creat table post

---
+++ database/migrations/1733176565891_create_posts_table.ts
@@ -0,0 +1,21 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'posts'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('slug').notNullable()
+      table.string('title').notNullable()
+      table.text('description').nullable()
+      table.integer('event_id').unsigned().references('id').inTable('events').onDelete('CASCADE').notNullable();
+      table.timestamp('created_at')
+      table.timestamp('updated_at').nullable()
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
