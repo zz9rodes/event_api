5ac3865d86db1701bb7d7b12c9c523a049c01fb0
rodes nzie <rodesnzie@gmail.com>
2024-12-02 01:24:26 +0100

    create events migrations

---
+++ database/migrations/1733093023843_create_alter_table_events_add_feild_slugs_table.ts
@@ -0,0 +1,18 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'events'
+
+  async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('slug').notNullable()
+    })
+  }
+
+  async down() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropColumn('slug')
+    })
+  }
+  
+}
