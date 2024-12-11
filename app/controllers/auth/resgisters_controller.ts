40b03b4727c3691656c4d8eef8adf9b4d3ea3e3f
rodes nzie <rodesnzie@gmail.com>
2024-12-11 10:04:32 +0100

    fix authentification bug

--- app/controllers/auth/resgisters_controller.ts
+++ app/controllers/auth/resgisters_controller.ts
@@ -15,10 +15,14 @@ export default class ResgistersController {
             const  ResponseData :ApiResponse= await this.RegisterService.Register(payload)
             response.status(ResponseData.status).json(ResponseData)
         } catch (error) {
+            console.log(error);
+            
             if (error instanceof errors.E_VALIDATION_ERROR) {
-                response.status(442).json(ApiResponse.error(442,error.messages))
-              }
-              response.status(500).json(error)
+                response.status(422).json(ApiResponse.error(422,error.messages))
+            }
+           else {
+            response.status(500).json(ApiResponse.error(500, 'Erreur interne du serveur'))
+         }
         }
     }
 }
